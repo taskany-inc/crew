c20c7ea544512b0cdc41aa7f6eec755b761f1d54
tsumo <tsumo@users.noreply.github.com>
2023-07-31 11:39:06 +0300

    chore(trpc): initialize trpc

---
+++ src/pages/api/trpc/[trpc].ts
@@ -0,0 +1,9 @@
+import * as trpcNext from '@trpc/server/adapters/next';
+
+import { trpcRouter } from '../../../trpc/router';
+import { createContext } from '../../../trpc/context';
+
+export default trpcNext.createNextApiHandler({
+    router: trpcRouter,
+    createContext,
+});
---
+++ src/trpc/router/index.ts
@@ -0,0 +1,5 @@
+import { router } from '../trpcBackend';
+
+export const trpcRouter = router({});
+
+export type TrpcRouter = typeof trpcRouter;
