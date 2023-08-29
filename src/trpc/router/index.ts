440f36705b3822fed922f6447cc12296b2edf5cf
Katrinputrina <putrina.katya@gmail.ru>
2023-08-29 11:53:44 +0300

    feat: feedback button as in issues

--- src/trpc/router/index.ts
+++ src/trpc/router/index.ts
@@ -2,10 +2,12 @@ import { router } from '../trpcBackend';
 
 import { groupRouter } from './group-router';
 import { userRouter } from './user-router';
+import { feedbackRouter } from './feedback-router';
 
 export const trpcRouter = router({
     user: userRouter,
     group: groupRouter,
+    feedback: feedbackRouter,
 });
 
 export type TrpcRouter = typeof trpcRouter;
