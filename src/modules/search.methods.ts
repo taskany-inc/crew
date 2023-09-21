63d4ca4a37310194446724f46a76772a4346ccb6
Viktor Prokopyev <prokopyevviktor@yandex.ru>
2023-09-21 10:51:37 +0300

    feat: method for global search

---
+++ src/modules/search.methods.ts
@@ -0,0 +1,40 @@
+import { Group, User } from 'prisma/prisma-client';
+
+import { prisma } from '../utils/prisma';
+
+export const searchMethods = {
+    global: async (input: string): Promise<{ users: User[]; groups: Group[] }> => {
+        const [users, groups] = await Promise.all([
+            prisma.user.findMany({
+                take: 5,
+                where: {
+                    OR: [
+                        {
+                            email: {
+                                contains: input,
+                                mode: 'insensitive',
+                            },
+                        },
+                        {
+                            name: {
+                                contains: input,
+                                mode: 'insensitive',
+                            },
+                        },
+                    ],
+                },
+            }),
+            prisma.group.findMany({
+                take: 5,
+                where: {
+                    name: {
+                        contains: input,
+                        mode: 'insensitive',
+                    },
+                },
+            }),
+        ]);
+
+        return { users, groups };
+    },
+};
