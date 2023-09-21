1e4459d4d9197cbddc707b07623e5aca33a26bd6
Viktor Prokopyev <prokopyevviktor@yandex.ru>
2023-09-21 10:51:37 +0300

    feat: method for global search

--- src/trpc/router/index.ts
+++ src/trpc/router/index.ts
@@ -3,11 +3,13 @@ import { router } from '../trpcBackend';
 import { groupRouter } from './group-router';
 import { userRouter } from './user-router';
 import { feedbackRouter } from './feedback-router';
+import { searchRouter } from './search-router';
 
 export const trpcRouter = router({
     user: userRouter,
     group: groupRouter,
     feedback: feedbackRouter,
+    search: searchRouter,
 });
 
 export type TrpcRouter = typeof trpcRouter;
