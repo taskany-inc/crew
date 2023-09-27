ee83b92af91181fc97ed4dec06782ef66aadc8a7
Katrinputrina <putrina.katya@gmail.ru>
2023-09-27 20:07:14 +0300

    feat: redirect to my profile from root

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,12 @@
 import { pages } from '../hooks/useRouter';
+import { createGetServerSideProps } from '../utils/createGetSSRProps';
 
-export async function getServerSideProps() {
-    return { redirect: { destination: pages.teams } };
-}
+export const getServerSideProps = createGetServerSideProps({
+    requireSession: true,
+    action: ({ session }) => {
+        return { redirect: { destination: pages.user(session.user.id) } };
+    },
+});
 
 export default function HomePage() {
     return null;
