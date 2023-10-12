8ee56914e17d2f90833b3ee16e932252bf105b27
Katrinputrina <putrina.katya@gmail.ru>
2023-10-12 17:51:58 +0300

    fix(Previews): preview headers are inconsistent

--- src/components/Previews.tsx
+++ src/components/Previews.tsx
@@ -1,4 +1,6 @@
 import { nullable } from '@taskany/bricks';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 import { usePreviewContext } from '../context/preview-context';
 
@@ -6,7 +8,14 @@ import TeamProfilePreview from './groups/TeamProfilePreview';
 import UserProfilePreview from './users/UserProfilePreview';
 
 export const Previews = () => {
-    const { userId, group } = usePreviewContext();
+    const { userId, group, hidePreview } = usePreviewContext();
+    const router = useRouter();
+    const { pathname } = router;
+
+    useEffect(() => {
+        hidePreview();
+    }, [pathname]);
+
     return (
         <>
             {nullable(group, (g) => (
