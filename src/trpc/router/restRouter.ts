fd1bfc1c4a9fc822e27de41317b50b85de86e05a
Anton <sab94a@yandex.ru>
2024-02-14 15:06:51 +0300

    fix: rest api user image

--- src/trpc/router/restRouter.ts
+++ src/trpc/router/restRouter.ts
@@ -147,6 +147,7 @@ export const restRouter = router({
                             id: z.string(),
                             name: z.string().nullable(),
                             email: z.string(),
+                            image: z.string().nullable(),
                         }),
                         roles: z.array(
                             z.object({
