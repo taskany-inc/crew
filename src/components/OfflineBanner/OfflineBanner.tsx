b24b353433d4b7a464af6d3cd15dbf3a0a08e542
Katrinputrina <putrina.katya@gmail.ru>
2024-02-19 09:32:06 +0300

    feat(Harmony): use offline bar from bricks

--- src/components/OfflineBanner/OfflineBanner.tsx
+++ src/components/OfflineBanner/OfflineBanner.tsx
@@ -1,22 +1,8 @@
 import { onlineManager } from '@tanstack/react-query';
-import styled from 'styled-components';
-import { nullable, useOfflineDetector, Text } from '@taskany/bricks';
-import { gapS } from '@taskany/colors';
-import { IconExclamationCircleOutline } from '@taskany/icons';
+import { nullable, useOfflineDetector } from '@taskany/bricks';
+import { OfflineBanner as OfflineBannerBricks } from '@taskany/bricks/harmony';
 
 import { tr } from './OfflineBanner.i18n';
-
-const StyledBanner = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 50px;
-    background-color: #462322;
-    text-align: center;
-    gap: ${gapS};
-    bottom: 0;
-`;
 
 export const OfflineBanner = () => {
     const [globalOnlineStatus, remoteServerStatus] = useOfflineDetector({
@@ -26,11 +12,6 @@ export const OfflineBanner = () => {
         remoteServerUrl: '/api/health',
     });
     return nullable(!globalOnlineStatus || !remoteServerStatus, () => (
-        <StyledBanner>
-            <IconExclamationCircleOutline size="s" color="#f7c0be" />
-            <Text as="span" color="#f7c0be">
-                {tr('You are currently offline. Check connection.')}
-            </Text>
-        </StyledBanner>
+        <OfflineBannerBricks text={tr('You are currently offline. Check connection.')} />
     ));
 };
