814b4ad42e26d9b16ec0a9d2bc4ed3cb0aa9df53
Anton <sab94a@yandex.ru>
2024-02-29 00:01:17 +0300

    feat: config logo

--- src/components/LayoutMain.tsx
+++ src/components/LayoutMain.tsx
@@ -31,7 +31,9 @@ export const LayoutMain: FC<LayoutMainProps> = ({ pageTitle, children }) => {
     const fullTitle = pageTitle ? `${pageTitle} - Taskany Crew` : 'Taskany Crew';
 
     const userSettings = trpc.user.getSettings.useQuery();
-    const appConfig = trpc.appConfig.get.useQuery();
+    const appConfig = trpc.appConfig.get.useQuery(undefined, {
+        staleTime: Infinity,
+    });
 
     const { resolvedTheme } = useTheme();
     const theme = (
@@ -42,13 +44,13 @@ export const LayoutMain: FC<LayoutMainProps> = ({ pageTitle, children }) => {
         <>
             <Head>
                 <title>{fullTitle}</title>
-                <link rel="icon" href={appConfig.data?.logo ?? '/favicon.png'} />
+                <link rel="icon" href={appConfig.data?.favicon ?? '/favicon.png'} />
                 <link rel="stylesheet" id="themeVariables" href={`/theme/${theme}.css`} />
             </Head>
             <OfflineBanner />
 
             <GlobalStyle />
-            <PageHeader />
+            <PageHeader logo={appConfig.data?.logo ?? undefined} />
             <Theme theme={theme} />
             <StyledContent>{children}</StyledContent>
 
--- src/components/PageHeaderLogo.tsx
+++ src/components/PageHeaderLogo.tsx
@@ -1,15 +1,11 @@
-import { TaskanyLogo } from '@taskany/bricks';
+import { TaskanyLogo } from '@taskany/bricks/harmony';
 
 import { pages } from '../hooks/useRouter';
 
 import { Link } from './Link';
 
-export const PageHeaderLogo: React.FC = () => {
-    // TODO: resolve custom logo from settings in db
-
-    return (
-        <Link href={pages.home}>
-            <TaskanyLogo />
-        </Link>
-    );
-};
+export const PageHeaderLogo: React.FC<{ logo?: string }> = ({ logo }) => (
+    <Link href={pages.home}>
+        <TaskanyLogo src={logo} />
+    </Link>
+);
