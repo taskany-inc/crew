a2cc01e1390f83c3cdff0afb5c3cc2ea98c0d28e
Katrinputrina <putrina.katya@gmail.ru>
2024-03-28 14:24:58 +0300

    fix(OfflineBanner): infinity requests at health checks

--- src/components/OfflineBanner/OfflineBanner.tsx
+++ src/components/OfflineBanner/OfflineBanner.tsx
@@ -1,3 +1,4 @@
+import { useRef } from 'react';
 import { onlineManager } from '@tanstack/react-query';
 import { nullable, useOfflineDetector } from '@taskany/bricks';
 import { OfflineBanner as OfflineBannerBricks } from '@taskany/bricks/harmony';
@@ -5,11 +6,15 @@ import { OfflineBanner as OfflineBannerBricks } from '@taskany/bricks/harmony';
 import { tr } from './OfflineBanner.i18n';
 
 export const OfflineBanner = () => {
+    const isOnline = useRef<boolean | null>(null);
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const [_, remoteServerStatus] = useOfflineDetector({
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         setStatus: ([_global, remote]) => {
-            onlineManager.setOnline(remote);
+            if (isOnline.current !== remote) {
+                isOnline.current = remote;
+                onlineManager.setOnline(remote);
+            }
         },
         remoteServerUrl: '/api/health',
     });
