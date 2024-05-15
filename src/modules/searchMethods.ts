eb706e4bc628d59ac278788db9c43518c41e8f46
DenisVorop <saskeazaske@icloud.com>
2024-05-15 14:57:22 +0300

    feat: add rest route to global search users

--- src/modules/searchMethods.ts
+++ src/modules/searchMethods.ts
@@ -3,27 +3,57 @@ import { Group, User } from 'prisma/prisma-client';
 import { prisma } from '../utils/prisma';
 
 export const searchMethods = {
-    global: async (input: string): Promise<{ users: User[]; groups: Group[] }> => {
-        const [users, groups] = await Promise.all([
-            prisma.user.findMany({
-                take: 5,
-                where: {
-                    OR: [
-                        {
-                            email: {
-                                contains: input,
-                                mode: 'insensitive',
-                            },
-                        },
-                        {
-                            name: {
-                                contains: input,
-                                mode: 'insensitive',
-                            },
-                        },
-                    ],
+    globalUsers: async (input: string, translit = ''): Promise<User[]> => {
+        return prisma.user.findMany({
+            take: 5,
+            orderBy: {
+                _relevance: {
+                    fields: ['name', 'email', 'login'],
+                    // https://github.com/prisma/prisma/issues/8939#issuecomment-933990947
+                    search: input.replace(/[\s\n\t]/g, '_'),
+                    sort: 'asc',
                 },
-            }),
+            },
+            where: {
+                OR: [
+                    {
+                        name: {
+                            contains: translit,
+                            mode: 'insensitive',
+                        },
+                    },
+                    {
+                        name: {
+                            contains: input,
+                            mode: 'insensitive',
+                        },
+                    },
+                    {
+                        email: {
+                            contains: input,
+                            mode: 'insensitive',
+                        },
+                    },
+                    {
+                        login: {
+                            contains: translit,
+                            mode: 'insensitive',
+                        },
+                    },
+                    {
+                        login: {
+                            contains: input,
+                            mode: 'insensitive',
+                        },
+                    },
+                ],
+            },
+        });
+    },
+
+    global: async (input: string, translit = ''): Promise<{ users: User[]; groups: Group[] }> => {
+        const [users, groups] = await Promise.all([
+            searchMethods.globalUsers(input, translit),
             prisma.group.findMany({
                 take: 5,
                 where: {
