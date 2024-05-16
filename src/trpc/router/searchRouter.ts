3108f36bf895ccb564d88f268e037bdd83ed4571
Viktor Prokopyev <prokopyevviktor@yandex.ru>
2024-05-16 20:41:34 +0300

    fix(searchRouter): translit in global search

--- src/trpc/router/searchRouter.ts
+++ src/trpc/router/searchRouter.ts
@@ -1,10 +1,12 @@
 import { z } from 'zod';
+import { translit } from '@taskany/bricks';
 
 import { protectedProcedure, router } from '../trpcBackend';
 import { searchMethods } from '../../modules/searchMethods';
 
 export const searchRouter = router({
     global: protectedProcedure.input(z.string()).query(({ input }) => {
-        return searchMethods.global(input);
+        const translitInput = translit(input);
+        return searchMethods.global(input, translitInput);
     }),
 });
