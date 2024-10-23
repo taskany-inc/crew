e356ae6d68f50ddfbcefe91dd9f75802f1d3e971
Viktor Prokopyev <prokopyevviktor@yandex.ru>
2024-10-23 16:13:11 +0300

    feat(INTERNAL-1275): decompose InternalUserCreationPage

---
+++ src/components/TeamBlockUserForm/TeamBlockUserForm.tsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import { Text } from '@taskany/bricks/harmony';
+import { useFormContext } from 'react-hook-form';
+import { Group, User } from 'prisma/prisma-client';
+
+import { FormControl } from '../FormControl/FormControl';
+import { UserSelect } from '../UserSelect/UserSelect';
+import { GroupComboBox } from '../GroupComboBox/GroupComboBox';
+import { CreateUserCreationRequestInternalEmployee } from '../../modules/userCreationRequestSchemas';
+import { Nullish } from '../../utils/types';
+
+import s from './TeamBlockUserForm.module.css';
+import { tr } from './TeamBlockUserForm.i18n';
+
+interface TeamBlockUserFormProps {
+    className: string;
+    id: string;
+}
+
+export const TeamBlockUserForm = ({ className, id }: TeamBlockUserFormProps) => {
+    const {
+        setValue,
+        trigger,
+        watch,
+        formState: { errors },
+    } = useFormContext();
+
+    const onUserChange = (user: Nullish<User>, type: keyof CreateUserCreationRequestInternalEmployee) => {
+        user && setValue(type, user.id);
+        trigger(type);
+    };
+
+    const onTeamChange = (group: Nullish<Group>) => {
+        group && setValue('groupId', group.id);
+        trigger('groupId');
+    };
+
+    return (
+        <div className={className} id={id}>
+            <Text className={s.SectionHeader} weight="bold" size="lg">
+                {tr('Team')}
+            </Text>
+            <div className={s.TwoInputsRow}>
+                <FormControl label={tr('Supervisor')} required>
+                    <UserSelect
+                        mode="single"
+                        selectedUsers={watch('supervisorId') ? [watch('supervisorId')] : undefined}
+                        onChange={(users) => onUserChange(users[0], 'supervisorId')}
+                        error={errors.supervisorId}
+                    />
+                </FormControl>
+                <FormControl label={tr('Line managers')}>
+                    <UserSelect
+                        mode="multiple"
+                        selectedUsers={watch('lineManagerIds')}
+                        onChange={(users) =>
+                            setValue(
+                                'lineManagerIds',
+                                users.map((user) => user.id),
+                            )
+                        }
+                    />
+                </FormControl>
+            </div>
+
+            <div className={s.ThreeInputsRow}>
+                <FormControl label="Buddy">
+                    <UserSelect
+                        mode="single"
+                        selectedUsers={watch('buddyId') ? [watch('buddyId')] : undefined}
+                        onChange={(users) => onUserChange(users[0], 'buddyId')}
+                        onReset={() => setValue('buddyId', undefined)}
+                    />
+                </FormControl>
+                <FormControl label={tr('OrgGroup')}>
+                    <GroupComboBox
+                        defaultGroupId={watch('groupId')}
+                        onChange={onTeamChange}
+                        error={errors.groupId}
+                        onReset={() => setValue('groupId', undefined)}
+                    />
+                </FormControl>
+                <FormControl label={tr('Coordinators')}>
+                    <UserSelect
+                        mode="multiple"
+                        selectedUsers={watch('coordinatorIds')}
+                        onChange={(users) =>
+                            setValue(
+                                'coordinatorIds',
+                                users.map((user) => user.id),
+                            )
+                        }
+                    />
+                </FormControl>
+            </div>
+        </div>
+    );
+};
