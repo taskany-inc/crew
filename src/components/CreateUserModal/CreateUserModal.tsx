2d81b4ce15a05eefa3b0986a0da6a69a24180431
Viktor Prokopyev <prokopyevviktor@yandex.ru>
2024-10-30 16:33:53 +0300

    feat(INTERNAL-1292): modal to user request forms

--- src/components/CreateUserModal/CreateUserModal.tsx
+++ src/components/CreateUserModal/CreateUserModal.tsx
@@ -1,4 +1,11 @@
-import { FormTitle, Modal, ModalContent, ModalCross, ModalHeader } from '@taskany/bricks';
+import { useState } from 'react';
+import { nullable } from '@taskany/bricks';
+import { ModalHeader, Modal, ModalContent, Text, Button } from '@taskany/bricks/harmony';
+import cn from 'classnames';
+import { IconTickCircleOutline } from '@taskany/icons';
+
+import { useRouter } from '../../hooks/useRouter';
+import { config } from '../../config';
 
 import s from './CreateUserModal.module.css';
 import { tr } from './CreateUserModal.i18n';
@@ -8,15 +15,89 @@ interface CreateUserModalProps {
     onClose: VoidFunction;
 }
 
+type RequestPage =
+    | 'newInternalUserRequest'
+    | 'newExternalUserRequest'
+    | 'newExternalFromMainUserRequest'
+    | 'newExistingUserRequest';
+
+interface RequestTypeListType {
+    name: string;
+    description: string;
+    page: RequestPage;
+}
+
 export const CreateUserModal = ({ visible, onClose }: CreateUserModalProps) => {
+    const [active, setActive] = useState<RequestPage | undefined>();
+
+    const requestTypeList: RequestTypeListType[] = [
+        {
+            name: tr('Create a planned newcommer'),
+            description: tr('Informing about the departure of a new employee'),
+            page: 'newInternalUserRequest',
+        },
+        {
+            name: tr('Create access to employee from {mainOrgName} (external)', {
+                mainOrgName: config.mainOrganizationName || 'Main',
+            }),
+            description: tr('external from main description {mainOrgName}', {
+                mainOrgName: config.mainOrganizationName || 'Main',
+            }),
+            page: 'newExternalFromMainUserRequest',
+        },
+        {
+            name: tr('Access to external employees'),
+            description: tr('external  description'),
+            page: 'newExternalUserRequest',
+        },
+        {
+            name: tr('Access employee'),
+            description: tr('existing  description'),
+            page: 'newExistingUserRequest',
+        },
+    ];
+
+    const router = useRouter();
     return (
-        <Modal visible={visible} onClose={onClose} width={600} className={s.Modal}>
+        <Modal visible={visible} onClose={onClose} width={530}>
             <ModalHeader>
-                <FormTitle>{tr('Create request for user creation')}</FormTitle>
-                <ModalCross onClick={onClose} />
+                <Text weight="bold">{tr('Which request you want to create?')}</Text>
             </ModalHeader>
 
-            <ModalContent className={s.ModalContent}></ModalContent>
+            <ModalContent>
+                {requestTypeList.map(({ name, description, page }) => (
+                    <div className={s.RequestType} key={page}>
+                        <div>
+                            <Text
+                                className={cn(s.Text, {
+                                    [s.Text_active]: active === page,
+                                })}
+                                size="m"
+                                weight="thin"
+                                onClick={() => setActive(page)}
+                            >
+                                {name}
+                            </Text>
+                            <Text size="xs" weight="thin" className={s.Description}>
+                                {description}
+                            </Text>
+                        </div>
+                        {nullable(active === page, () => (
+                            <IconTickCircleOutline size="s" className={s.IconTick} />
+                        ))}
+                    </div>
+                ))}
+                <div className={s.FormActions}>
+                    <Button type="button" text={tr('Cancel')} onClick={onClose} />
+                    <Button
+                        type="button"
+                        view="primary"
+                        text={tr('Create')}
+                        onClick={() => active && router[active]()}
+                        disabled={!active}
+                    />
+                </div>
+            </ModalContent>
         </Modal>
     );
 };
