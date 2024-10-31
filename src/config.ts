f659e63d3883c225e35796a85b54243adab7b633
Viktor Prokopyev <prokopyevviktor@yandex.ru>
2024-10-31 15:51:35 +0300

    feat(INTERNAL-1300): org/users groups tabs

--- src/config.ts
+++ src/config.ts
@@ -58,4 +58,5 @@ export const config = {
         : 4,
     mainOrganizationId: process.env.NEXT_PUBLIC_MAIN_ORG_ID || '1',
     mainOrganizationName: process.env.NEXT_PUBLIC_MAIN_ORG_NAME,
+    orgGroupId: process.env.NEXT_PUBLIC_ORG_GROUP_ID,
 };
