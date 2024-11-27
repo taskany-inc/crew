af06141544e6a8cafbd19d60af91efe07d1943ad
Viktor Prokopyev <prokopyevviktor@yandex.ru>
2024-11-27 11:14:18 +0300

    fix(INTERNAL-1395): email for external service

--- src/worker/resolve.ts
+++ src/worker/resolve.ts
@@ -1,60 +1,40 @@
-import * as Sentry from '@sentry/nextjs';
-
 import { historyEventMethods } from '../modules/historyEventMethods';
 import { userMethods } from '../modules/userMethods';
 
 import { JobDataMap } from './create';
 
 export const scheduledDeactivation = async ({ userId }: JobDataMap['scheduledDeactivation']) => {
-    try {
-        await userMethods.editActiveState({ id: userId, active: false });
-        await historyEventMethods.create({ subsystem: 'Scheduled profile deactivate' }, 'editUserActiveState', {
-            userId,
-            groupId: undefined,
-            before: true,
-            after: false,
-        });
-    } catch (error) {
-        Sentry.captureException(error, {
-            fingerprint: ['worker', 'resolve'],
-        });
-    }
+    await userMethods.editActiveState({ id: userId, active: false });
+    await historyEventMethods.create({ subsystem: 'Scheduled profile deactivate' }, 'editUserActiveState', {
+        userId,
+        groupId: undefined,
+        before: true,
+        after: false,
+    });
 };
 
 export const createProfile = async ({ userCreationRequestId }: JobDataMap['createProfile']) => {
-    try {
-        const user = await userMethods.createUserFromRequest(userCreationRequestId);
+    const user = await userMethods.createUserFromRequest(userCreationRequestId);
 
-        const phone = user.services.find((service) => service.serviceName === 'Phone')?.serviceId;
+    const phone = user.services.find((service) => service.serviceName === 'Phone')?.serviceId;
 
-        await historyEventMethods.create({ subsystem: 'Scheduled profile creation' }, 'createUser', {
-            userId: user.id,
-            groupId: undefined,
-            before: undefined,
-            after: {
-                name: user.name || undefined,
-                email: user.email,
-                phone,
-                login: user.login || undefined,
-                organizationalUnitId: user.organizationUnitId || undefined,
-                supervisorId: user.supervisorId || undefined,
-            },
-        });
-    } catch (error) {
-        Sentry.captureException(error, {
-            fingerprint: ['worker', 'resolve'],
-        });
-    }
+    await historyEventMethods.create({ subsystem: 'Scheduled profile creation' }, 'createUser', {
+        userId: user.id,
+        groupId: undefined,
+        before: undefined,
+        after: {
+            name: user.name || undefined,
+            email: user.email,
+            phone,
+            login: user.login || undefined,
+            organizationalUnitId: user.organizationUnitId || undefined,
+            supervisorId: user.supervisorId || undefined,
+        },
+    });
 };
 
 export const resolveDecree = async ({ userCreationRequestId }: JobDataMap['createProfile']) => {
-    try {
-        const user = await userMethods.resolveDecreeRequest(userCreationRequestId);
+    const user = await userMethods.resolveDecreeRequest(userCreationRequestId);
 
-        return user;
-    } catch (error) {
-        Sentry.captureException(error, {
-            fingerprint: ['worker', 'resolve'],
-        });
-    }
+    return user;
 };
